8b8e42571a0337b3fc3b88135471b37cac6cf951
vishu543 <vishumca51@gmail.com>
2026-05-02 01:32:13 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "s3dbtf"
+    key            = "ec2-new/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+    encrypt = true
+  }
+}
