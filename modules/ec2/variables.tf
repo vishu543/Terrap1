8833745bdc8281703ac8ddce439639812b7f6fa5
vishu543 <vishumca51@gmail.com>
2026-05-02 21:42:46 +0530

    m

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,7 +1,8 @@
 variable "amiid"{
     type=string
+    default="ami-0c101f26f147fa7fd" 
 }
 variable "instance_type"{
     type=string
-    
+    default="t3.micro"
 }
---
+++ prod/variables.tf
@@ -0,0 +1,8 @@
+variable "amiid"{
+    type=string
+    default="ami-0c101f26f147fa7fd" 
+}
+variable "instance_type"{
+    type=string
+    default="t2.micro"
+}
